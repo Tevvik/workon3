cac1055688244ad68f06760a67253431a3388353
Patrykjpg <60928214+Patrykjpg@users.noreply.github.com>
2022-01-04 18:25:38 +0100

    Add files via upload

---
+++ p-home.php
@@ -0,0 +1,178 @@
+<?php 
+/*
+Template name: Strona główna
+*/
+get_header();
+?>
+<!-- hero -->
+<header class="hero">
+        <div>
+            <h1>Pokaż swój styl</h1>
+            <a class="a_btn" href="#">Sprzedaj teraz</a>
+            <a href="#">Zobacz jak to działa</a>
+        </div>
+    </header>
+    <!-- main -->
+    <main>
+        <!-- section sneak peek under hero -->
+        <section class="container_sneak_peek">
+            <h2>Popularne produkty</h2>
+            <div  class="sneak_peek">
+                <section class="sneak_peek_wrapper">
+                    <img src="http://localhost/loveoutfits/wp-content/uploads/2022/01/sneak-peek.jpg" alt="Przedmiot z oferty">
+                    <h4>29,00 zł</h4>
+                    <h4>rozmiar</h4>
+                    <h4>marka</h4>
+                </section>
+                <section class="sneak_peek_wrapper">
+                    <img src="http://localhost/loveoutfits/wp-content/uploads/2022/01/sneak-peek.jpg" alt="Przedmiot z oferty">
+                    <h4>29,00 zł</h4>
+                    <h4>rozmiar</h4>
+                    <h4>marka</h4>
+                </section>
+                <section class="sneak_peek_wrapper">
+                    <img src="http://localhost/loveoutfits/wp-content/uploads/2022/01/sneak-peek.jpg" alt="Przedmiot z oferty">
+                    <h4>29,00 zł</h4>
+                    <h4>rozmiar</h4>
+                    <h4>marka</h4>
+                </section>
+                <section class="sneak_peek_wrapper">
+                    <img src="http://localhost/loveoutfits/wp-content/uploads/2022/01/sneak-peek.jpg" alt="Przedmiot z oferty">
+                    <h4>29,00 zł</h4>
+                    <h4>rozmiar</h4>
+                    <h4>marka</h4>
+                </section>
+            </div>
+        </section>
+        <section class="container">
+            <h2>Znajdź swoją ulubioną markę</h2>
+            <div class="sneak_peek_bottom">
+                <ul>
+                    <li><a href="#" class="a_btn">Nike</a></li>
+                    <li><a href="#" class="a_btn">Addidas</a></li>
+                    <li><a href="#" class="a_btn">Zara</a></li>
+                    <li><a href="#" class="a_btn">Gino Rossi</a></li>
+                    <li><a href="#" class="a_btn">Marka</a></li>
+                    <li><a href="#" class="a_btn">Marka krzak</a></li>
+                    <li><a href="#" class="a_btn">czerwony dywan</a></li>
+                    <li><a href="#" class="a_btn">złoty dywan</a></li>
+                    <li><a href="#" class="a_btn">alpaka inc.</a></li>
+                </ul>
+            </div>
+        </section>
+        <!-- section about us home -->
+        <section class="container_about">
+            <h2>Dlaczego warto u nas kupować?</h2>
+            <div class="container_p">
+                <div class="about_us_home">
+                    <div class="wrapper_column">
+                        <i class="icon_about fas fa-cart-arrow-down"></i>
+                        <h3>Najlepsze oferty</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum</p>
+                    </div>
+                    <div class="wrapper_column">
+                        <i class="icon_about fas fa-truck-moving"></i>
+                        <h3>Prosta wysyłka</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum</p>
+                    </div>
+                    <div class="wrapper_column">
+                        <i class="icon_about fas fa-search"></i>
+                        <h3>Szeroki wybór</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum</p>
+                    </div>
+                    <div class="wrapper_column">
+                        <i class="icon_about fas fa-shield-alt"></i>
+                        <h3>Bezpieczeństwo</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!-- hero -->
+<header class="hero">
+        <div>
+            <h1>Pokaż swój styl</h1>
+            <a class="a_btn" href="#">Sprzedaj teraz</a>
+            <a href="#">Zobacz jak to działa</a>
+        </div>
+    </header>
+    <!-- main -->
+    <main>
+        <!-- section sneak peek under hero -->
+        <section class="container_sneak_peek">
+            <h2>Popularne produkty</h2>
+            <div  class="sneak_peek">
+                <section class="sneak_peek_wrapper">
+                    <img src="img/sneak-peek.jpg" alt="Przedmiot z oferty">
+                    <h4>29,00 zł</h4>
+                    <h4>rozmiar</h4>
+                    <h4>marka</h4>
+                </section>
+                <section class="sneak_peek_wrapper">
+                    <img src="img/sneak-peek.jpg" alt="Przedmiot z oferty">
+                    <h4>29,00 zł</h4>
+                    <h4>rozmiar</h4>
+                    <h4>marka</h4>
+                </section>
+                <section class="sneak_peek_wrapper">
+                    <img src="img/sneak-peek.jpg" alt="Przedmiot z oferty">
+                    <h4>29,00 zł</h4>
+                    <h4>rozmiar</h4>
+                    <h4>marka</h4>
+                </section>
+                <section class="sneak_peek_wrapper">
+                    <img src="img/sneak-peek.jpg" alt="Przedmiot z oferty">
+                    <h4>29,00 zł</h4>
+                    <h4>rozmiar</h4>
+                    <h4>marka</h4>
+                </section>
+            </div>
+        </section>
+        <section class="container">
+            <h2>Znajdź swoją ulubioną markę</h2>
+            <div class="sneak_peek_bottom">
+                <ul>
+                    <li><a href="#" class="a_btn">Nike</a></li>
+                    <li><a href="#" class="a_btn">Addidas</a></li>
+                    <li><a href="#" class="a_btn">Zara</a></li>
+                    <li><a href="#" class="a_btn">Gino Rossi</a></li>
+                    <li><a href="#" class="a_btn">Marka</a></li>
+                    <li><a href="#" class="a_btn">Marka krzak</a></li>
+                    <li><a href="#" class="a_btn">czerwony dywan</a></li>
+                    <li><a href="#" class="a_btn">złoty dywan</a></li>
+                    <li><a href="#" class="a_btn">alpaka inc.</a></li>
+                </ul>
+            </div>
+        </section>
+        <!-- section about us home -->
+        <section class="container_about">
+            <h2>Dlaczego warto u nas kupować?</h2>
+            <div class="container_p">
+                <div class="about_us_home">
+                    <div class="wrapper_column">
+                        <i class="icon_about fas fa-cart-arrow-down"></i>
+                        <h3>Najlepsze oferty</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum</p>
+                    </div>
+                    <div class="wrapper_column">
+                        <i class="icon_about fas fa-truck-moving"></i>
+                        <h3>Prosta wysyłka</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum</p>
+                    </div>
+                    <div class="wrapper_column">
+                        <i class="icon_about fas fa-search"></i>
+                        <h3>Szeroki wybór</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum</p>
+                    </div>
+                    <div class="wrapper_column">
+                        <i class="icon_about fas fa-shield-alt"></i>
+                        <h3>Bezpieczeństwo</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+<?php include 'footer.php';?>
+<?php get_footer(); ?>

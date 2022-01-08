0e8d7c1af48aecf9bffa58f9d7d54bd7e909dc25
Ju <julia.kolano66@gmail.com>
2022-01-08 16:27:54 +0100

    s

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-                <link rel="stylesheet" href="<?=get_template_directory()."\dist\main.css"?>">
+                <link rel="stylesheet" href="<?=get_template_directory_uri()."\dist\main.css"?>">
                 <title><?php bloginfo('name'); wp_title('|');?></title>
                 
                 <?php wp_head();?>
